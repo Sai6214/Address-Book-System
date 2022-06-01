58cc040d9c4931d39222cc15d404be938134c864
Saikumar <muthyalasaikumar9@gmail.com>
2022-06-01 21:07:54 +0530

    commit

---
+++ Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace AddressBookSystem
+{
+    internal class Person
+    {
+        public string First_Name;
+        public string Last_Name;
+        public string Address;
+        public string City;
+        public string State;
+        public long Zip_Code;
+        public long Phone_Number;
+        public string Email_ID;
+
+        public string Result()
+        {
+            return "First Name: " + First_Name + "\n Last name: " + Last_Name + "\n Address: " + Address + "\n City: " + City +
+            "\n State:" + State + "\n Zop COde: " + Zip_Code + "\n Phone Number: " + Phone_Number + "/n Email ID: " + Email_ID;
+        }
+    }
+
+    internal class AddressBook
+    {
+
+        public List<Person> person = new List<Person>();
+
+        public void Display()
+        {
+            foreach (Person per in person)
+            {
+                Console.WriteLine(per.Result());
+            }
+        }
+
+        public void AddPerson(Person p)
+        {
+            person.Add(p);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"C:\Users\HP\BridgeLabzs\Assignment\Day-10\AddressBookSystem\AddressBookSystem\AddressBookResult.json", jsonData);
+        }
+        public AddressBook()
+        {
+            string json = File.ReadAllText(@"C:\Users\HP\BridgeLabzs\Assignment\Day-10\AddressBookSystem\AddressBookSystem\AddressBookResult.json");
+            if (json.Length > 0)
+            {
+                person = JsonConvert.DeserializeObject<List<Person>>(json);
+            }
+            else
+            {
+                person = new List<Person>();
+            }
+        }
+        public void EditPersonDetails(string First_Name)
+        {
+            for (int i = 0; i < person.Count; i++)
+            {
+                if (person[i].First_Name == First_Name)
+                {
+                    Console.WriteLine("Enter The First Name: ");
+                    person[i].First_Name = Console.ReadLine();
+                    Console.WriteLine("Enter The Last Name: ");
+                    person[i].Last_Name = Console.ReadLine();
+                    Console.WriteLine("Enter The Address ");
+                    person[i].Address = Console.ReadLine();
+                    Console.WriteLine("Enter The City: ");
+                    person[i].City = Console.ReadLine();
+                    Console.WriteLine("Enter The State: ");
+                    person[i].State = Console.ReadLine();
+                    Console.WriteLine("Enter The Zip Code: ");
+                    person[i].Zip_Code = long.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter The Phone Number: ");
+                    person[i].Phone_Number = long.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter The Email ID: ");
+                    person[i].Email_ID = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Only Same Name Required ");
+                }
+            }
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"C:\Users\HP\BridgeLabzs\Assignment\Day-10\AddressBookSystem\AddressBookSystem\AddressBookResult.json", jsonData);
+        }
+
+        public void RemovePersonDetails(string First_Name)
+        {
+            Person persn = null;
+            foreach (Person del in person)
+            {
+                if (del.First_Name == First_Name)
+                {
+                    persn = del;
+                }
+            }
+            person.Remove(persn);
+            string jsonData = JsonConvert.SerializeObject(person);
+            File.WriteAllText(@"D:\.Net\Assignment\day10\AddressBookManagement\AddressBookManagement\result.json", jsonData);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace AddressBookSystem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------- WELCOME TO THE ADDRESS BOOK PROGRAM -----------------");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            AddressBook add = new AddressBook();
+            Person person = new Person();
+            person.First_Name = "PRAFUL";
+            person.Last_Name = "RAKHADE";
+            person.Address = "WARD NO.2 NEAR RAJESH MEDICAL KHAT";
+            person.City = "NAGPUR";
+            person.State = "MAHARASHTRA";
+            person.Zip_Code = 441106;
+            person.Phone_Number = 7038008002;
+            person.Email_ID = "prafulrakhade02@gmail.com";
+            add.AddPerson(person);
+            add.Display();
+            Console.WriteLine("=================================================================================");
+
+
+            AddressBook manage = new();
+            Person per = new Person();
+            Console.WriteLine("=====USER INPUT=======");
+            Console.Write("Enter First Name: ");
+            string firstName = Console.ReadLine();
+            per.First_Name = firstName;
+            Console.WriteLine();
+            Console.Write("Enter the last Name: ");
+            string lastName = Console.ReadLine();
+            per.Last_Name = lastName;
+            Console.WriteLine();
+            Console.Write("Enter the Address: ");
+            string address = Console.ReadLine();
+            per.Address = address;
+            Console.WriteLine();
+            Console.Write("Enter the City: ");
+            string city = Console.ReadLine();
+            per.City = city;
+            Console.WriteLine();
+            Console.Write("Enter the State: ");
+            string state = Console.ReadLine();
+            per.State = state;
+            Console.WriteLine();
+            Console.Write("Enter the Zip Code: ");
+            long zipCode = long.Parse(Console.ReadLine());
+            per.Zip_Code = zipCode;
+            Console.WriteLine();
+            Console.Write("Enter the Phone Number: ");
+            long phoneNumber = long.Parse(Console.ReadLine());
+            per.Phone_Number = phoneNumber;
+            Console.WriteLine();
+            Console.Write("Enter the Email ID: ");
+            string email = Console.ReadLine();
+            per.Email_ID = email;
+            Console.WriteLine();
+            manage.Display();
+            manage.AddPerson(per);
+            Console.WriteLine("============================================================");
+
+
+            AddressBook edit = new();
+            edit.Display();
+            Console.WriteLine("Enter The Name For Edit Contact Details: ");
+            Console.WriteLine();
+            string Name1 = Console.ReadLine();
+            edit.EditPersonDetails(Name1);
+            edit.Display();
+            Console.WriteLine("============================================================");
+
+
+            AddressBook delete = new AddressBook();
+            delete.Display();
+            Console.WriteLine("Enter The Name For Delete the Contact Details: ");
+            string Name2 = Console.ReadLine();
+            delete.RemovePersonDetails(Name2);
+            delete.Display();
+            Console.WriteLine("============================================================");
+        }
+    }
+}
